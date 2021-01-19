29faa63ed9d0159da707c90a5eaa8390b8f5c99c
jyajoo <jyajoo1020@gmail.com>
2021-01-19 21:15:32 +0900

    달팽이는 올라가고 싶다

--- 2869.py
+++ 2869.py
@@ -1,19 +1,3 @@
-# #달팽이는 올라가고 싶다
-
-# a, b, v = map(int, input().split(' '))
-
-# cnt = meter = 0
-
-# while True:
-#     meter += a
-#     if meter >= v:
-#         cnt += 1
-#         break
-#     meter -= b
-#     cnt += 1
-
-# print(cnt)
-
 #달팽이는 올라가고 싶다
 
 a, b, v = map(int, input().split(' '))
@@ -21,14 +5,24 @@ a, b, v = map(int, input().split(' '))
 cnt = meter = 0
 
 while True:
-    i = v // a
-    if i == 0 or v%a==0:
+    meter += a
+    if meter >= v:
         cnt += 1
         break
-    gap = (a-b) * i
-    v -= gap
-    cnt += i
-    if v<=0:
-        break
+    meter -= b
+    cnt += 1
 
 print(cnt)
+
+#달팽이는 올라가고 싶다_2
+
+a, b, v = map(int, input().split(' '))
+
+day = (v-a)/(a-b)
+
+if day == int(day):
+    day = int(day)
+else:
+    day = int(day) + 1
+
+print(day+1)
