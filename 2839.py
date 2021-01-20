c69c3d5e99e37db85c096c9d9d27cadad69371c2
jyajoo <jyajoo1020@gmail.com>
2021-01-20 23:12:56 +0900

    설탕 배달

--- 2839.py
+++ 2839.py
@@ -1,4 +1,4 @@
-#설탕 배달
+# #설탕 배달
 
 n = num = int(input())
 
@@ -20,4 +20,20 @@ while True:
                 cnt = -1
                 break
     
+print(cnt)
+##############################################
+#설탕 배달_2
+
+n = int(input())
+cnt = 0
+while True:
+    if n % 5 == 0:
+        cnt += n//5
+        break
+    else:
+        n = n-3
+        if n<0:
+            cnt = -1
+            break
+        cnt += 1
 print(cnt)
