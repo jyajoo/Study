9cc4dc0e287ab9d6ffba0f755cda911056a41ad4
jyajoo <jyajoo1020@gmail.com>
2021-01-28 19:28:21 +0900

    소수 찾기

--- 1978.py
+++ 1978.py
@@ -8,7 +8,7 @@ count = 0 #소수의 개수
 for i in m:
     cnt = 0
     if i == 1: #소수가 아님
-        continue #아래 코드를 실행하지 않고 건너뜀!!!
+        continue #아래 코드를 실행하지 않고 건너뜀!!! #?또는 cnt +=1을 해준다.
     
     for j in range(2, i): #2부터 i-1까지 차례대로 나누기
         if i%j == 0: #나머지가 0이면 소수가 아님!
