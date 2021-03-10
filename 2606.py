a60fd6ba2ee5c69c811c8e6ebdb2fb8901b367d0
jyajoo <jyajoo1020@gmail.com>
2021-03-10 11:24:54 +0900

    바이러스

--- 2606.py
+++ 2606.py
@@ -2,7 +2,7 @@
 
 n = int(input())  # 컴퓨터의 수(정점의 개수)
 m = int(input())  # 연결된 쌍의 수(간선의 개수)
-graph = [[0]*(n+1) for _ in range(n+1)]  # 인덱스값을 간선으로 생각
+graph = [[0]*(n+1) for _ in range(n+1)]  # 인덱스값을 정점으로 생각
 visited = [False] * (n+1)  # 정점을 방문하였는지
 count = 0  # 바이러스 걸린 컴퓨터의 수
 
@@ -31,15 +31,15 @@ print(count-1)  # 1번 컴퓨터 제외
 ####################################################################
 # 바이러스 (dfs 깊이우선탐색)
 
-n = int(input())  # 컴퓨터의 수(정점의 개수)
-m = int(input())  # 연결된 쌍의 수(간선의 개수)
-graph = [[0]*(n+1) for _ in range(n+1)]  # 인덱스값을 간선으로 생각
-visited = [False] * (n+1)  # 정점을 방문하였는지
-count = 0  # 바이러스 걸린 컴퓨터의 수
+n = int(input())
+m = int(input())
+graph = [[0]*(n+1) for _ in range(n+1)]
+visited = [False] * (n+1)
+count = 0
 
 for i in range(m):
     a, b = map(int, input().split(' '))
-    graph[a][b] = graph[b][a] = 1  # 둘 사이가 연결되어있다면 1
+    graph[a][b] = graph[b][a] = 1
 
 
 def DFS(start):
