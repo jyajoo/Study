b6103e9d730b9aa70dc00046e1e85a985203b4d6
jyajoo <jyajoo1020@gmail.com>
2021-07-05 14:27:44 +0900

    숫자의 개수

--- 2577.py
+++ 2577.py
@@ -1,4 +1,4 @@
-#숫자의 개수
+# 숫자의 개수
 
 a = int(input())
 b = int(input())
@@ -13,8 +13,8 @@ for i in mul:
 for i in num:
     print(i)
 
-######################################
-#숫자의 개수_2
+#####################################
+# 숫자의 개수_2
 
 a = int(input())
 b = int(input())
@@ -24,3 +24,19 @@ mul = str(a*b*c)
 
 for i in range(10):
     print(mul.count(str(i)))
+
+#####################################
+# 숫자의 개수_3
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = str(a * b * c)
+
+for i in range(0, 10):
+    cnt = 0
+    for j in result:
+        if(int(j) == i):
+            cnt += 1
+    print(cnt)
