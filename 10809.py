50c54974c4ed73560a1b495eb9b339a08680816b
jyajoo <jyajoo1020@gmail.com>
2021-07-07 16:00:24 +0900

    알파벳 찾기

--- 10809.py
+++ 10809.py
@@ -1,13 +1,13 @@
-#알파벳 찾기
+# 알파벳 찾기
 
 S = input()
 alphabet = list(range(97, 123))
 
 for i in alphabet:
-    print(S.find(chr(i)), end = " ")
+    print(S.find(chr(i)), end=" ")
 
 ###########################################
-#알파벳 찾기_2
+# 알파벳 찾기_2
 
 S = list(input())
 alphabet = list("abcdefghijklmnopqrstuvwxyz")
@@ -20,4 +20,19 @@ for i in range(len(alphabet)):
         result.insert(i, -1)
 
 for i in result:
-    print(i, end = " ")
+    print(i, end=" ")
+
+###########################################
+# 알파벳 찾기_3
+s = input()
+
+a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for i in a:
+    for j in range(len(s)):
+        if (i == s[j]):
+            print(j, end=' ')
+            break
+    if(i not in s):
+        print(-1, end=' ')
