44eb4eb8ca97ddac12add973dc6d38ee4d379e6c
jyajoo <jyajoo1020@gmail.com>
2021-07-07 17:35:24 +0900

    단어 공부

--- 1157.py
+++ 1157.py
@@ -1,13 +1,13 @@
-#단어 공부
+# 단어 공부
 
 word = input().upper()
 
-alphabet = {} #딕셔너리
+alphabet = {}  # 딕셔너리
 
 for i in word:
     if i in alphabet:
         alphabet[i] += 1
-    
+
     else:
         alphabet[i] = 1
 result = []
@@ -18,5 +18,20 @@ for key, value in alphabet.items():
 if len(result) >= 2:
     print("?")
 
+else:
+    print(result[0])
+
+############################################
+# 단어 공부_2
+# from collections import Counter
+
+a = dict(Counter(input().upper()))
+result = []
+for i in a.keys():
+    if a[i] == max(a.values()):
+        result.append(i)
+
+if len(result) > 1:
+    print("?")
 else:
     print(result[0])
