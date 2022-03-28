01b18a337e41d317d54ccb1f55666152c7009aa7
jyajoo <jyajoo1020@gmail.com>
2022-03-28 17:06:58 +0900

    feat: ChatBot - 말 배우기 기능 추가

--- Web-Javascript/ChatBot/index.js
+++ Web-Javascript/ChatBot/index.js
@@ -1,35 +1,70 @@
 var chat = document.getElementsByClassName('answer');
 var body = document.getElementsByTagName('body');
+var app = document.getElementById('typed');
+var app2 = document.getElementById('teach');
 var n = 0;
 var follow = 0;
+var key = 0;
+var question = "";
+var answer = "";
 
-var app = document.getElementById('typed');
 var typewriter = new Typewriter(app, {
     loop: false
 });
-
 var typewriter2 = new Typewriter(chat[0], {
     loop: false
 })
+var typewriter3 = new Typewriter(app2, {
+    loop: false
+});
+
+var json = [
+    {
+        "question": "안녕",
+        "answer": "안녕!!"
+    },
+    {
+        "question": "이름",
+        "answer": "스누피라고 해!"
+    }
+];
 
 typewriter.callFunction(function (state) {
     state.elements.cursor.style.display = "none"
     })
     .start();
 
-chatText("안녕! 하고 싶은 말 있니?")
+typewriter3.callFunction(function (state) {
+    state.elements.cursor.style.display = "none"
+    })
+    .start();
+
+chatText("안녕! 하고 싶은 말 있니?");
 
 function check_text() {
     var value = document.getElementById("console").value;
     console.log(value);
 
-    if (value.includes("안녕")) {
-        chatText("안녕!!")
+    if (key == 1) {
+        if (value.includes("그래")) {
+            chatText("대답을 입력해줘!");
+            key = 2;
+        }
+        else {
+            chatText("안녕! 하고 싶은 말 있니?");
+            key = 0;
+        }
+        return;
+    }   
+
+    if (key == 2) {
+        answer = value;
+        push_join();
+        return;
     }
-    else if (value.includes("이름")) {
-        chatText("스누피라고 해!")
-    }
-    else if (value.includes("뭐해") || value.includes("뭐하고")) {
+
+
+    if (value.includes("뭐해") || value.includes("뭐하고")) {
         chatText("하늘을 보고 있어~");
     }
     else if (value.includes("안아")) {
@@ -43,14 +78,14 @@ function check_text() {
             n++;
         }
         else {
-            body[0].style.backgroundImage = "url('/ChatBot/snoopy.webp')"
+            body[0].style.backgroundImage = "url('/ChatBot/snoopy.webp')";
             chatText("다시 누울래. 너도 이리 누워");
             n = 0;
         }
     }
     else if (value.includes("하늘")) {
         chatText("매일 하늘을 눈에 담고 있어");
-        body[0].style.backgroundImage = "url('/ChatBot/snoopy.webp')"
+        body[0].style.backgroundImage = "url('/ChatBot/snoopy.webp')";
     }
     else if (value.includes("따라해")) {
         chatText("그래 알겠어!");
@@ -66,17 +101,33 @@ function check_text() {
         }
     }
     else {
-        chatText("응? 뭐라고?");
+        for (let i = 0; i < json.length; i++){
+            if (value.includes(json[i].question)) {
+                chatText(json[i].answer);
+                return;
+            }
+        }
+        chatText("응? 뭐라고? 말을 가르쳐줘");
 
-        typewriter.typeString("키워드를 참고해보세요!")
-            .pauseFor(500)
+        typewriter.pauseFor(4500)
+            .typeString("키워드를 참고해보세요!")
+            .pauseFor(4000)
             .deleteAll()
             .callFunction(function (state) {
                 state.elements.cursor.style.display = "none"
             })
             .start();
-    }
-    
+        typewriter3.pauseFor(4500)
+            .typeString("말을 가르치고 싶다면 " + '"' + "그래" + '"' + " 입력")
+            .pauseFor(3500)
+            .deleteAll()
+            .callFunction(function (state) {
+                state.elements.cursor.style.display = "none"
+            })
+            .start();
+        key = 1;
+        question = value;
+    }    
 }
 
 function chatText(x) {
@@ -88,4 +139,11 @@ function chatText(x) {
             state.elements.cursor.style.display = "none"
         })
         .start();
+}
+
+function push_join() {
+    json.push({ question: `${question}`, answer: `${answer}` });
+
+    chatText("말을 배웠어 고마워!");
+    key = 0;
 }
