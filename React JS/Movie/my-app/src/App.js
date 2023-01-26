5aff81d48bc7c1b6090735920c25420d92a1eb02
jyajoo <jyajoo1020@gmail.com>
2023-01-26 17:39:01 +0900

    [ReactJS] useEffect

--- React JS/Movie/my-app/src/App.js
+++ React JS/Movie/my-app/src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => console.log("i run only once");
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Continue</button>
     </div>
   );
 }
