289b6757fdb9514827dea520eb57324e06bf6963
jyajoo <jyajoo1020@gmail.com>
2023-01-26 21:05:38 +0900

    [ReactJS] clean-up function

--- React JS/Movie/my-app/src/App.js
+++ React JS/Movie/my-app/src/App.js
@@ -1,31 +1,20 @@
-import styles from "./App.module.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("i run all the time");
-  useEffect(() => console.log("I run only once."), []);
-  useEffect(() => console.log("I run when 'keyword' changes."), [keyword]);
-  useEffect(() => console.log("I run when 'counter' changes"), [counter]);
-  useEffect(
-    () => console.log("I run when keyword & counter change"),
-    [keyword, counter]
-  );
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Continue</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
