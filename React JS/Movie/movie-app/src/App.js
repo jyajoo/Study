31b519761a1b10aae5d23531e27e4dcf4442bc0d
jyajoo <jyajoo1020@gmail.com>
2023-02-02 20:57:36 +0900

    [ReactJS] Movie - Router 설정, Link 추가

--- React JS/Movie/movie-app/src/App.js
+++ React JS/Movie/movie-app/src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
