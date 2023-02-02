09aa0f5eb8d8838a15862398c6e71296f7bf1941
jyajoo <jyajoo1020@gmail.com>
2023-02-02 21:46:40 +0900

    [ReactJS] Movie - Detail 연결

--- React JS/Movie/movie-app/src/components/MovieDetail.js
+++ React JS/Movie/movie-app/src/components/MovieDetail.js
@@ -1,29 +1,37 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function MovieDetail({
+  id,
+  coverImg,
+  title,
+  year,
+  runtime,
+  genres,
+  rating,
+  description_full,
+}) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
+      <h2>{title}</h2>
+      <p>{year}</p>
+      <p>runtime : {runtime}</p>
+      <p>rating : {rating}</p>
       <ul>
         {genres.map((genre) => (
           <li key={genre}>{genre}</li>
         ))}
       </ul>
+      <p>{description_full}</p>
     </div>
   );
 }
 
-Movie.propTypes = {
+MovieDetail.propTypes = {
   id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default Movie;
+export default MovieDetail;
--- React JS/Movie/movie-app/src/routes/Detail.js
+++ React JS/Movie/movie-app/src/routes/Detail.js
@@ -1,5 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetail";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
+  const getMovies = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    console.log(json);
+    setLoading(false);
+  }, [id]);
+  useEffect(() => {
+    getMovies();
+  }, [getMovies]);
+  return (
+    <div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <MovieDetail
+            key={movie.id}
+            id={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            year={movie.year}
+            runtime={movie.runtime}
+            genres={movie.genres}
+            rating={movie.rating}
+            description_full={movie.description_full}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
