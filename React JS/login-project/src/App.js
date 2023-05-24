94252a01a950220904d7951c0f00c64c1e4f8205
jyajoo <jyajoo1020@gmail.com>
2023-05-24 22:19:08 +0900

    [ReactJS] 리코일 재적용

--- React JS/login-project/src/App.js
+++ React JS/login-project/src/App.js
@@ -2,15 +2,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-// 상태 관리, 전역 변수 관리, 컴포넌트간 데이터 공유
-import {
-  RecoilRoot,
-  atom,
-  useRecoilState,
-  useRecoilValue,
-  selector,
-} from "recoil";
-
 // 전역 변수 중 일부를 영속적으로 로컬 스토리지에 저장
 import { recoilPersist } from "recoil-persist";
 
@@ -24,9 +15,6 @@ import {
   useParams,
   useNavigate,
 } from "react-router-dom";
-
-// useEffect 없이 API 통신이 가능하도록
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 // 통신 API
 import axios from "axios";
@@ -46,7 +34,6 @@ import styles from "./css/App.module.css";
 
 const persistAtom = recoilPersist();
 const axiosInstance = axios.create();
-const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -63,14 +50,4 @@ function App() {
   );
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 export default App;
--- React JS/login-project/src/index.js
+++ React JS/login-project/src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// 상태 관리, 전역 변수 관리, 컴포넌트간 데이터 공유
+import {
+  RecoilRoot,
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  selector,
+} from "recoil";
+
+// useEffect 없이 API 통신이 가능하도록
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
