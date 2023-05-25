e6c22f6abb48f11cf27b46c966b54248a3de3a23
jyajoo <jyajoo1020@gmail.com>
2023-05-25 22:03:57 +0900

    [ReactJS] NavBar 추가

--- React JS/login-project/src/components/Header.js
+++ React JS/login-project/src/components/Header.js
@@ -1,28 +1,23 @@
-import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    // <ul className="flex shadow">
-    //   <li>
-    //     <Nav className="p-4 block" to="/home/main">
-    //       홈
-    //     </Nav>
-    //   </li>
-    //   <li>
-    //     <NavLink className="p-4 block" to="/account/login">
-    //       로그인
-    //     </NavLink>
-    //   </li>
-    // </ul>
-    <Nav className="justify-content-end" activeKey="/home/main">
-      <Nav.Item>
-        <Nav.Link href="/home/main">HOME</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/account/login">LOGIN</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="/home/main">BOOTREPORT</Navbar.Brand>
+        <Nav className="justify-text-end">
+          <Nav.Link as={NavLink} to="/home/main" href="/home/main">
+            HOME
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/account/login" href="/account/login">
+            LOGIN
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
